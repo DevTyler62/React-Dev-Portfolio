5d69977a9230bcc85978799b02a86ffc4ed3b03c
DevTyler62 <smithtj921@gmail.com>
2026-09-06 00:15:25 -0400

    Update Tic Tac Toe project logo and screenshot

--- src/containers/work/imports.js
+++ src/containers/work/imports.js
@@ -2,7 +2,7 @@ import Buggy from "../../../src/assets/buggy-logo.png";
 import Church from "../../../src/assets/church-logo.png";
 import Datafy from "../../../src/assets/datafy-logo.png";
 import Skywind from "../../../src/assets/skywind-logo.png";
-import TicTacToe from "../../../src/assets/tictactoe-logo.png";
+import TicTacToe from "../../../src/assets/tictactoe-logo-new.png";
 import Todo from "../../../src/assets/todo-logo.png";
 import Extra from "../../../src/assets/manager.svg";
 import Productivity from "../../../src/assets/logosaas.png";
